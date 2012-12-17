ebf7b6658672a6c113349119ccd9762e554e7bae
agilesonic <agilesonic@gmail.com>
2012-12-17 00:31:02 -0500

    fixed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -108,14 +108,14 @@ Hadean::Application.configure do
     #)
 
     ::GATEWAY = ActiveMerchant::Billing::AuthorizeNetGateway.new(
-      :login    => Settings.authnet.login
-      :password => Settings.authnet.login
+      :login    => Settings.authnet.login,
+      :password => Settings.authnet.login,
       :test     => true
     )
 
     ::CIM_GATEWAY = ActiveMerchant::Billing::AuthorizeNetCimGateway.new(
-      :login    => Settings.authnet.login
-      :password => Settings.authnet.password
+      :login    => Settings.authnet.login,
+      :password => Settings.authnet.password,
       :test     => true
     )
 
